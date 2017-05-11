7d0d659eb58fffd322ca69dc7830a3e239aca148
Vikas Jain <vksjn18@gmail.com>
2017-05-11 19:45:56 +0530

    update functions completed, functions for calculating expectation of latent variables left

--- src/model_new.py
+++ src/model_new.py
@@ -27,8 +27,17 @@ def initialize(m_opts):
 
     m_vars['U'] = m_opts['init_std']*np.random.randn(m_vars['n_users'], m_vars['n_components']).astype(floatX)
     m_vars['Ub'] = np.zeros((m_vars['batch_size'], m_vars['n_components'])).astype(floatX)
+
     m_vars['V'] = m_opts['init_std']*np.random.randn(m_vars['n_labels'], m_vars['n_components']).astype(floatX)
     m_vars['W'] = m_opts['init_W']*np.random.randn(m_vars['n_components'],m_vars['n_features']).astype(floatX)
+
+    # accumulator of sufficient statistics of label factors
+    m_vars['sigma_v'] = np.zeros((m_opts['n_labels'], m_opts['n_components'], m_opts['n_components']))
+    m_vars['x_v'] = np.zeros((m_opts['n_labels'], m_opts['n_components']))
+
+    # accumulator of sufficient statistics of W matrix
+    m_vars['sigma_W'] = np.zeros((m_vars['n_features'], m_vars['n_features']))
+    m_vars['x_W'] = np.zeros((m_vars['n_features'], m_vars['n_components']))
 
     if m_opts['observance']:
     	a0,b0 = m_opts['init_mu_a'],m_opts['init_mu_b']
@@ -47,8 +56,56 @@ def update(m_opts, m_vars):
 	return m_vars
 
 def update_U(m_opts, m_vars):
-	P = E_xi(m_opts, m_vars) # expectation of xi_{nl}
-	N = E_omega(m_opts, m_vars) # expecation of omega_{nl}
-	K = PG(m_opts, m_vars) # polyagamma kappa_{nl}
+	for i in range(m_opts['batch_size']):
+		for it in range(m_opts['PG_iters']):
+			P_i = E_xi_row(i, m_opts, m_vars) # expectation of xi_{nl} for n = i
+			N_i = E_omega_row(i, m_opts, m_vars) # expecation of omega_{nl} for n = i
+			K_i = PG_row(i, m_opts, m_vars) # polyagamma kappa_{nl} for n = i
+			PN_i = P_i*N_i
+			PK_i = P_i*K_i
+			sigma = m_vars['V'].T.dot(PN_i*m_vars['V']) + m_opts['lam_u']*np.eye(m_opts['n_components'])
+			x = m_vars['V'].T.dot(PK_i) + m_opts['lam_u']*m_vars['W'].dot(m_vars['Xb'][i])
+			m_vars['Ub'][i] = LA.solve(sigma, x)
 
-	sigma = 
+def update_V(m_opts, m_vars):
+	for i in range(m_vars['n_labels']):
+		P_i = E_xi_col(i, m_opts, m_vars) # expectation of xi_{nl} for l = i
+		N_i = E_omega_col(i, m_opts, m_vars) # expecation of omega_{nl} for l = i 
+		K_i = PG_col(i, m_opts, m_vars) # polyagamma kappa_{nl} for l = i
+		PN_i = P_i*N_i
+		PK_i = P_i*K_i
+
+		sigma = m_vars['Ub'].T.dot(PN_i*m_vars['Ub']) + m_opts['lam_v']*np.eye(m_opts['n_components'])
+		x = m_vars['Ub'].T.dot(PK_i)
+
+		m_vars['sigma_v'][i] = (1-m_vars['gamma'])*m_vars['sigma_v'][i] + gamma_ratio*sigma
+		m_vars['x_v'][i] = (1-m_vars['gamma'])*m_vars['x_v'][i] + gamma_ratio*x
+		m_vars['V'][i] = LA.solve(m_vars['sigma_v'][i], m_vars['x_v'][i])
+
+def update_observance(m_opts, m_vars):
+	P = E_xi(m_opts, m_vars)
+	P = P.sum(axis=0)
+	mu = (m_vars['a']+P-1)/(m_vars['a']+m_vars['b']+m_vars['batch_size']-2)
+    m_vars['mu'] = (1-m_opts['gamma'])*m_vars['mu'] + gamma_ratio*mu
+
+def update_W(m_opts, m_vars):
+
+	sigma = m_opts['lam_w']*(m_vars['Xb'].dot(m_vars['Xb']))
+	m_vars['sigma_W'] = (1-m_vars['gamma'])*m_vars['sigma_W'] + m_vars['gamma']*sigma
+
+	x = m_vars['Xb'].T.dot(m_vars['Ub'])
+	m_vars['x_W'] = (1-m_vars['gamma'])*m_vars['x_W'] + m_vars['gamma']*x
+
+	if m_opts['use_cg'] != True: # For the Ridge regression on W matrix with the closed form solutions 
+	    sigma = LA.inv(m_vars['sigma_W']) # O(N^3) time for N x N matrix inversion 
+	    m_vars = sigma.dot(m_vars['x_W'])
+
+	else: # For the CG on the ridge loss to calculate W matrix
+	    Y = m_vars['x_W']
+	    X = m_vars['sigma_W']
+	    for i in range(Y.shape[1]):
+	        y = Y[:, i]
+	        w,info = sp_linalg.cg(X, y, x0=self.W[i,:], maxiter=self.cg_iter)
+	        if info < 0:
+	            print "WARNING: sp_linalg.cg info: illegal input or breakdown"
+	        m_vars['W'][i, :] = w.T
