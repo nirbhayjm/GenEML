fa1fd1818040d4c7fd624422756d5a2b5a52aec8
Vikas Jain <vksjn18@gmail.com>
2017-06-06 04:45:31 +0530

    delicious sh file

---
+++ src/run_delicious.sh
@@ -0,0 +1,1 @@
+python main.py --dataset="../data/delicious_1.mat" -v -o -bs=1024 -n_components=200 -lam_u=1e-3 -lam_v=1e-3 -lam_w=1e-4 -use_cg=True -cg_iters=5 -PG_iters=5 -lr_alpha=1.0 -lr_tau=0.5 -init_mu=0.01
