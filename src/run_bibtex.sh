7fd409d094803448fd3f7bd0aa6260b2ae19a916
Vikas Jain <vksjn18@gmail.com>
2017-06-06 03:49:18 +0530

    added shell files

--- src/run_bibtex.sh
+++ src/run_bibtex.sh
@@ -1,1 +1,1 @@
-python main.py --dataset="../data/bibtex_new.mat" -v -observance=True -bs=1024 -n_components=200 -lam_u=1e-3 -lam_v=1e-3 -lam_w=1e-4 -use_cg=True -cg_iters=5 -PG_iters=5 -lr_alpha=0.6 -lr_tau=0.55 -init_mu=0.01
+python main.py --dataset="../data/bibtex_new.mat" -v -observance=True -bs=4880 -n_components=200 -lam_u=1e-3 -lam_v=1e-3 -lam_w=1e-4 -use_cg=True -cg_iters=5 -PG_iters=5 -lr_alpha=0.6 -lr_tau=0.55 -init_mu=0.01
---
+++ src/run_synthdata.sh
@@ -0,0 +1,1 @@
+python SD-exp1.py --dataset="../data/sdata.mat" -v -bs=500 -n_components=25 -lr_alpha=1.0 -lr_tau=0.
